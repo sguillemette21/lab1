15deb54be3c2100cab5bf6c87832a25cf36815ea
Sam Guillemette <samguillemette@Minn.localdomain>
2018-02-01 14:10:59 -0500

    Exercise 5

--- lab1.ml
+++ lab1.ml
@@ -83,7 +83,7 @@ manifest in the language's textual notation.
 ........................................................................
 Exercise 3: Consider the following abstract syntax tree:
 
-      -
+      ~-
       |
       |
       -
@@ -99,7 +99,7 @@ appropriate OCaml expression to assign the value to the variable
 exercise1 below.
 ......................................................................*)
 
-let exercise3 () = 3 - 5 ;;
+let exercise3 () = -(5 - 3) ;;
 
 (* Hint: The OCaml concrete expression
 
@@ -111,9 +111,16 @@ does *not* correspond to the abstract syntax above.
 Exercise 4: Draw the tree that the concrete syntax "- 5 - 3" does
 correspond to. Check it with a member of the course staff if you'd
 like.
-......................................................................*)
-
-   
+......................................................................
+      ~-
+      |
+      |
+      +
+      ^
+     / \
+    /   \
+   5     3
+  *)
 (*======================================================================
 Part 2: Types and type inference
 
@@ -124,24 +131,24 @@ expressions below? Test your solution by uncommenting the examples
 error is generated.
 ......................................................................*)
 
-(*   <--- remove this start of comment line
 
-let exercise5a : ??? = 42 ;;
 
-let exercise5b : ??? =
+let exercise5a : int = 42 ;;
+
+let exercise5b : string =
   let greet y = "Hello " ^ y
   in greet "World!";;
 
-let exercise5c : ???  =
+let exercise5c : int  =
   fun (x, y) -> x + int_of_float y ;;
 
-let exercise5d : ??? =
+let exercise5d : bool =
   fun x -> x < x + 1 ;;
 
-let exercise5e : ??? =
+let exercise5e : list =
   fun x -> if x then [x] else [] ;;
 
-remove this end of comment line too ----> *)
+
 
 (*======================================================================
 Part 3: First-order functional programming
